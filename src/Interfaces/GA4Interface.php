687a76121143ba39cac9da2689ae918ae7341b6d
Shiva Thapa <shiva.thapa@ensue.us>
2022-11-29 06:38:48 +0000

    GA4Interface.php edited online with Bitbucket

--- src/Interfaces/GA4Interface.php
+++ src/Interfaces/GA4Interface.php
@@ -4,7 +4,7 @@ namespace Ensue\GA4\Interfaces;
 
 interface GA4Interface
 {
-
+// TODO : add php doc or attribute 
     public function runReport(array $args): array;
 
     public function runBatchReport(array $args): array;
